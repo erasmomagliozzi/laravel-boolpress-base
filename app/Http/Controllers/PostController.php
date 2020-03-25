3fd31e463197bf02b74f4277cfb4944d8e023f9f
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-03-25 16:24:03 +0100

    commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Post;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
     }
 
     /**
